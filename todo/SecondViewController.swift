f0d41bd57380b712b89b7f487c08ace7887b4384
shunhuaiyao <shunhuai.yao@gmail.com>
2019-06-09 23:44:43 +0800

    Initial Commit

---
+++ todo/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  todo
+//
+//  Created by Yao Shun-Huai on 2019/6/9.
+//  Copyright © 2019 Yao Shun-Huai. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
